0e1c43595f1377609f9fa2d3db5e25cb0c112c40
Pedro <166235608+CommitSamurai@users.noreply.github.com>
2024-05-25 17:10:45 -0300

    Create playGame function
    The function plays 'Rock, Paper, Scissors' for 5 rounds.

--- rps.js
+++ rps.js
@@ -28,53 +28,67 @@ function getHumanChoice(){
 
     return humanChoice;
 }
- 
-function playRound(computerChoice, humanChoice){
-    switch(humanChoice){
-        case 'rock':
-            if (computerChoice === 'paper'){
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                computerScore++;
-            } else if (computerChoice === 'scissors'){
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                humanScore++;
-            } else{
-                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-            }
-        
-            break
-        
-        case 'paper':
-            if (computerChoice === 'scissors'){
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                computerScore++;
-            } else if (computerChoice === 'rock'){
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                humanScore++;
-            } else{
-                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-            }
-            
-            break;
-        
-        case 'scissors':
-            if (computerChoice === 'rock'){
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                computerScore++;
-            } else if (computerChoice === 'paper'){
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                humanScore++;
-            } else{
-                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-            }
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    i = 1;
 
-        break
+    function playRound(computerChoice, humanChoice){
+        switch(humanChoice){
+            case 'rock':
+                if (computerChoice === 'paper'){
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                    computerScore++;
+                } else if (computerChoice === 'scissors'){
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                    humanScore++;
+                } else{
+                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+                }
+            
+                break
+            
+            case 'paper':
+                if (computerChoice === 'scissors'){
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                    computerScore++;
+                } else if (computerChoice === 'rock'){
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                    humanScore++;
+                } else{
+                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+                }
+                
+                break;
+            
+            case 'scissors':
+                if (computerChoice === 'rock'){
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                    computerScore++;
+                } else if (computerChoice === 'paper'){
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                    humanScore++;
+                } else{
+                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+                }
+    
+            break
+        }
+
+        if(humanScore > computerScore){
+            console.log(`You win! You beat the computer a total of ${humanScore} times!`)
+        } else if (humanScore < computerScore){
+            console.log(`You lost. You beat the computer a total of ${humanScore} times.`)
+        } else{
+            console.log(`It's a draw! You beat the computer a total of ${humanScore} times.`)
+        }
+    }
+
+    while (i <= 5){
+        i++;
+        playRound(getComputerChoice(), getHumanChoice());
+        
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-playRound(getComputerChoice(), getHumanChoice())
-console.log(humanScore)
-console.log(computerScore)
+playGame()
