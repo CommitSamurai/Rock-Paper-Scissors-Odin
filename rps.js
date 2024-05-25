2924f0feddd3d327241e5d17ef7290c8d31a91e4
Pedro <166235608+CommitSamurai@users.noreply.github.com>
2024-05-25 16:40:53 -0300

    Add getHumanChoice function
    This function will input the user to choose rock, paper or scissors.

--- rps.js
+++ rps.js
@@ -4,3 +4,27 @@ function getComputerChoice(){
     return choices[randomChoices];
 }
 
+function getHumanChoice(){
+    let validchoice = false;
+    const textchoice = 'Rock, paper or scissors?';
+    let humanChoice = undefined;
+    
+    while (validchoice === false){
+    humanChoice = prompt(textchoice).toLowerCase();
+      switch(humanChoice){
+        case 'rock':
+        case 'paper':
+        case 'scissors':
+            validchoice = true;
+            break
+        default:
+            console.log('Invalid choice. Please select a valid option.')
+      }
+
+    
+    }
+
+    return humanChoice;
+}
+
+getHumanChoice();
