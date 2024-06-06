2681176cee911e9a5a3cf222050c61cf8d53ccf2
Pedro <166235608+CommitSamurai@users.noreply.github.com>
2024-06-06 12:53:17 -0300

    Remove 'playgame' function
    Transform 'Rock, paper, scissors' prompt into
    buttons

--- rps.js
+++ rps.js
@@ -27,59 +27,51 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-function playGame(){
-    let humanScore = 0;
+function playRound(computerChoice, humanChoice){
     let computerScore = 0;
-    i = 1;
-
-    function playRound(computerChoice, humanChoice){
-        switch(humanChoice){
-            case 'rock':
-                if (computerChoice === 'paper'){
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                    computerScore++;
-                } else if (computerChoice === 'scissors'){
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                    humanScore++;
-                } else{
-                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-                }
-            
-                break
-            
-            case 'paper':
-                if (computerChoice === 'scissors'){
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                    computerScore++;
-                } else if (computerChoice === 'rock'){
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                    humanScore++;
-                } else{
-                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-                }
-                
-                break;
-            
-            case 'scissors':
-                if (computerChoice === 'rock'){
-                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-                    computerScore++;
-                } else if (computerChoice === 'paper'){
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                    humanScore++;
-                } else{
-                    console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
-                }
+    let humanScore = 0;
     
+    switch(humanChoice){
+        case 'rock':
+            if (computerChoice === 'paper'){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerScore++;
+            } else if (computerChoice === 'scissors'){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanScore++;
+            } else{
+                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+            }
+        
             break
-        }
-    }
+        
+        case 'paper':
+            if (computerChoice === 'scissors'){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerScore++;
+            } else if (computerChoice === 'rock'){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanScore++;
+            } else{
+                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+            }
+            
+            break;
+        
+        case 'scissors':
+            if (computerChoice === 'rock'){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerScore++;
+            } else if (computerChoice === 'paper'){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanScore++;
+            } else{
+                console.log(`It's a draw! ${computerChoice} is equal to ${humanChoice}`)
+            }
 
-    while (i <= 5){
-        i++;
-        playRound(getComputerChoice(), getHumanChoice());
+        break
     }
-
+}
     if(humanScore > computerScore){
         console.log(`You win! You beat the computer a total of ${humanScore} times!`)
     } else if (humanScore < computerScore){
@@ -87,6 +79,12 @@ function playGame(){
     } else{
         console.log(`It's a draw! You beat the computer a total of ${humanScore} times.`)
     }
-}
 
-playGame()
+
+let rock = document.createElement('button')
+let paper = document.createElement('button')
+let scissors = document.createElement('button')
+
+rock.addEventListener('click', playRound(getComputerChoice, 'rock'))
+paper.addEventListener('click', playRound(getComputerChoice, 'paper'))
+scissors.addEventListener('click', playRound(getComputerChoice, 'scissors'))
